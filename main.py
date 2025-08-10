a78bc11f7d06e1a1ab2ae98245f87ebd883d082b
FrizzTradezMain <adrianj7769@outlook.com>
2025-08-10 01:56:03 -0700

    Added routs

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # Serve Html Content and Jinga Templates
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
